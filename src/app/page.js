b5c68dd2f6bc05c6fa804c49f3507abe4532b9cb
Muntasir Alam <alammuntasir01@gmail.com>
2025-09-07 00:34:44 +0600

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/Dark_light";
+
+export default function page() {
+  return (
+    <>
+    <ModeToggle/>
+    <h1 className="text-red-500 dark:text-white"> hello shaeadfdfdf cN</h1>
+    </>
+  )
+}
